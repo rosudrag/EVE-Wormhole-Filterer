697293eabbefecacb158698cbb3179028389a41e
Dragos Rosu <r.dragosh@yahoo.com>
2016-05-14 23:30:08 +0100

    feat: add flexboxgrid

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(express.static(__dirname + '/public'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.use('/css', express.static(__dirname + '/node_modules/flexboxgrid/css'));
 
 app.set('port', (process.env.PORT || 5000));
 
