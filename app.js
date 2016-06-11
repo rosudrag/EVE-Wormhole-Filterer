ded1a842380462e93a94d6e08c2c75bde77f91be
Dragos Rosu <r.dragosh@yahoo.com>
2016-06-11 16:04:32 +0100

    feat: extract myEVEoj

--- app.js
+++ app.js
@@ -6,50 +6,8 @@ var _iodash_ = require('lodash');
 var request = require('request');
 var $ = require('jquery');
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var myEVEoj = require("./myEVEoj");
 
-//EVEoj
-var EVEoj = require("EVEoj"),
-    SDD = EVEoj.SDD.Create("json", {
-        path: "\SDD_YC118_5_201605310"
-    }),
-    map;
-var solarSystems = {};
-SDD.LoadMeta().then(function() {
-    map = EVEoj.map.Create(SDD, "K");
-    return map.Load();
-}).then(function() {
-  solarSystems = Object.keys(map.GetSystems().map.sysNameMap);
-});
-
-function findSystemDistance(source, destination){
-  try{
-    var sourcesystem = map.GetSystem({name: source});
-    var destinationsystem = map.GetSystem({name: destination});
-    var route = map.Route(sourcesystem.ID, destinationsystem.ID, [], false, false);
-    return {
-      source: source,
-      destination: destination,
-      jumps: route.length
-    };
-  }
-  catch(ex){
-    return {
-      source: source,
-      destination: destination,
-      jumps: "N/A"
-    };
-  }
-}
-
-function findTradeHubDistances(source){
-  var results = {};
-  results["Jita"] = findSystemDistance(source, "Jita");
-  results["Amarr"] = findSystemDistance(source, "Amarr");
-  results["Hek"] = findSystemDistance(source, "Hek");
-  results["Rens"] = findSystemDistance(source, "Rens");
-  return results;
-}
-//--EVEoj
 
 var app = express();
 app.set('views', './views');
@@ -78,7 +36,7 @@ app.get('/solarsystems.json', function(req, res) {
 });
 
 app.get('/trade-hub-distance', function(req, res) {
-  var results = findTradeHubDistances("Jita");
+  var results = myEVEoj.findTradeHubDistances("Jita");
   res.render('tradehubdistancepage', {
     title: 'Thera Wormhole Filter App',
     distances: results
@@ -93,7 +51,7 @@ app.post('/trade-hub-distance', urlencodedParser, function(req, res) {
    if(source === undefined || source === ''){
      source = "Jita";
    }
-  var results = findTradeHubDistances(source);
+  var results = myEVEoj.findTradeHubDistances(source);
   res.render('tradehubdistancepage', {
     title: 'Thera Wormhole Filter App',
     distances: results
---
+++ myEVEoj.js
@@ -0,0 +1,52 @@
+// someThings.js
+
+(function() {
+  //EVEoj
+  var EVEoj = require("EVEoj"),
+      SDD = EVEoj.SDD.Create("json", {
+          path: "\SDD_YC118_5_201605310"
+      }),
+      map;
+  var solarSystems = {};
+  SDD.LoadMeta().then(function() {
+      map = EVEoj.map.Create(SDD, "K");
+      return map.Load();
+  }).then(function() {
+    solarSystems = Object.keys(map.GetSystems().map.sysNameMap);
+  });
+
+  function findSystemDistance(source, destination){
+    try{
+      var sourcesystem = map.GetSystem({name: source});
+      var destinationsystem = map.GetSystem({name: destination});
+      var route = map.Route(sourcesystem.ID, destinationsystem.ID, [], false, false);
+      return {
+        source: source,
+        destination: destination,
+        jumps: route.length
+      };
+    }
+    catch(ex){
+      return {
+        source: source,
+        destination: destination,
+        jumps: "N/A"
+      };
+    }
+  }
+
+  function findTradeHubDistances(source){
+    var results = {};
+    results["Jita"] = findSystemDistance(source, "Jita");
+    results["Amarr"] = findSystemDistance(source, "Amarr");
+    results["Hek"] = findSystemDistance(source, "Hek");
+    results["Rens"] = findSystemDistance(source, "Rens");
+    return results;
+  }
+  //--EVEoj
+
+    module.exports.findTradeHubDistances = function() {
+        return findTradeHubDistances(source);
+    }
+
+}());
